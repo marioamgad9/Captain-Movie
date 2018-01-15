996d351670c7fd354c1918e5d189ae0ece6054db
Mario Amgad <marioamgad9@gmail.com>
2018-01-15 23:22:05 +0200

    Replace anim folder with Java animations

--- app/src/main/java/com/mouris/mario/captainmovie/UI/MainActivity.java
+++ app/src/main/java/com/mouris/mario/captainmovie/UI/MainActivity.java
@@ -3,8 +3,6 @@ package com.mouris.mario.captainmovie.UI;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -12,6 +10,10 @@ import com.mouris.mario.captainmovie.R;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static final int ANIMATION_DURATION = 600;
+
+    private boolean inCaptainMode = true;
 
     private Button mAskCaptainButton;
     private ImageView mCaptainImageView;
@@ -24,5 +26,33 @@ public class MainActivity extends AppCompatActivity {
 
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
 
+        mAskCaptainButton = findViewById(R.id.askCaptainButton);
+        mCaptainImageView = findViewById(R.id.captain);
+
+        mAskCaptainButton.setOnClickListener(v->{
+            if (inCaptainMode) {
+                goToMovieMode();
+            } else {
+                goToCaptainMode();
+            }
+        });
+
+    }
+
+    private void goToCaptainMode() {
+        inCaptainMode = true;
+        //Show the captain
+        mCaptainImageView.animate().translationY(0).setDuration(ANIMATION_DURATION);
+        mCaptainImageView.animate().alpha(1).setDuration(ANIMATION_DURATION);
+        mAskCaptainButton.animate().translationY(0).setDuration(ANIMATION_DURATION);
+    }
+
+    private void goToMovieMode() {
+        inCaptainMode = false;
+        //Hide the captain
+        mCaptainImageView.animate().translationY(1250).setDuration(ANIMATION_DURATION);
+        mCaptainImageView.animate().alpha(0).setDuration(ANIMATION_DURATION);
+        //Show the askCaptainButton
+        mAskCaptainButton.animate().translationY(200).setDuration(ANIMATION_DURATION);
     }
 }
