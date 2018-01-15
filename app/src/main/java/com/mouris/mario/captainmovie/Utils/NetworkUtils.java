d1b54f487ce1ae66f19489659a632d6ad53b80fa
Mario Amgad <marioamgad9@gmail.com>
2018-01-15 14:14:41 +0200

    Add NetworkUtils class to check for internet connectivity

---
+++ app/src/main/java/com/mouris/mario/captainmovie/Utils/NetworkUtils.java
@@ -0,0 +1,16 @@
+package com.mouris.mario.captainmovie.Utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtils {
+    private NetworkUtils() {
+    }
+
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+}
