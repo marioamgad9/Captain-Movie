cf0a2dd85ae95f6cd783f283125bfddcb7ec8f5f
Mario Amgad <marioamgad9@gmail.com>
2018-01-16 18:33:12 +0200

    - Add a new app icon - Add a splash screen

--- app/src/main/java/com/mouris/mario/captainmovie/UI/MainActivity.java
+++ app/src/main/java/com/mouris/mario/captainmovie/UI/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
 
         MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
